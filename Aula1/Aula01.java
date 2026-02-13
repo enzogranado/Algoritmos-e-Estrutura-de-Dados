6eed402cb62cbce0299d7bd704c5db128176cfb2
ENZO CHAGAS GRANADO <enzochagas09granado@gmail.com>
2026-02-13 12:53:18 -0300

    feat: implement array logic

--- Aula1/Aula01.java
+++ Aula1/Aula01.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Aula01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        Scanner teclado = new Scanner(System.in);
-        int numero; 
-        System.out.println("Digite um numero: ");
-        numero = teclado.nextInt();
-        if (numero%2==0){
-            System.out.println("O numero é par " + numero);
-        } else{
-            System.out.println("o numero é impar " + numero);
+        try (Scanner teclado = new Scanner(System.in)) {
+            int numero; 
+            System.out.println("Digite um numero: ");
+            numero = teclado.nextInt();
+            if (numero%2==0){
+                System.out.println("O numero é par " + numero);
+            } else{
+                System.out.println("o numero é impar " + numero);
+            }
         }
     }
 }
--- Aula1/Vetores.java
+++ Aula1/Vetores.java
@@ -35,11 +35,45 @@ public class Vetores {
     public boolean estaVazio3() {
         return ocupacao == 0;
     }
+
     public boolean estavazio4() {
         return ocupacao == 0 ? true : false;
-        // se for = 0 retorna true, se nao, retornaa 0 
+        // se for = 0 retorna true, se nao, retornaa 0
     }
-    public boolean estaCheio(){
-        return ocupacao == capacidade; 
+
+    public boolean estaCheio() {
+        return ocupacao == capacidade;
+    }
+
+    public boolean adiciona(int elemento) {
+        if (estaCheio())
+            return false;
+        else {
+            vetor[ocupacao] = elemento;
+            // também podemos fazer assim: vetor[ocupacao++] = elemento;
+            // dessa forma podemos omitir a linha de ocupaççao ++
+            // colocamos a ocupação pois ela significa a ultima posição, nesse caso, a
+            // posição 0 já que
+            // nn tem nenhum elemento no array
+            ocupacao++;
+            // colocamos o ++ para garantir que o próximo elemento estará na proxima ocupação, próxima casinha
+            return true;
+        }
+    }
+    public int remove () {
+        if (estaVazio1()) return -1; // carefull
+        ocupacao--;
+        return vetor[ocupacao]; 
+        // A OCUPAÇÃO É BASICAMENTE A POSIÇÃO DO VALOR QUE SERÁ ADICIONADO OU RETIRADO
+        // NESSE CASO, A OCUPAÇÃO FOI REDUZIDA PARA "APONTAR" PARA O ELEMENTO QUE QUEREMOS RETIRAR
+        // POSTERIORMENTE RETIRAMOS ESSE ELEMENTO 
+    }
+    @Override
+    public String toString (){
+        if (estaVazio3()) return "vetor vazio";
+        String s = "";
+        for (int i = 0; i < ocupacao; i++)
+            s += vetor[i] + " ";
+        return s; 
     }
 }
